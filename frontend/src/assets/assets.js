44a6c224f5b6382a90aa0bdac6f3d7216327cfa3
adib422 <adibali422@gmail.com>
2025-10-14 14:47:43 +0530

    Add all project files

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,8 @@
+import cart_icon from './cart-plus-solid-full.svg'
+import search_icon from './search-solid.svg'
+import logo from './logo.png'
+export const assets = {
+    logo,
+    cart_icon,
+    search_icon
+}
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+    const [menu,setMenu] = useState("Home");
+
+  return (
+    <div className='navbar'>
+        <img src={assets.logo} alt="" className="logo" />
+        <ul className="navbar-menu">
+            <li onClick={()=>setMenu("Home")} className={menu=="Home"?"active":""}>Home</li>
+            <li onClick={()=>setMenu("Categories")} className={menu=="Categories"?"active":""}>Categories</li>
+            <li onClick={()=>setMenu("Contact us")} className={menu=="Contact us"?"active":""}>Contact us</li>
+            <li onClick={()=>setMenu("About us")} className={menu=="About us"?"active":""}>About us</li>
+        </ul>
+        <div className="navbar-right">
+        <div className="search-bar">
+          <input type="text" placeholder="Search medicines..." />
+          <button className="search-btn">
+            <img src={assets.search_icon} className="icon" alt="Search" />
+          </button>
+        </div>
+
+        <div className="navbar-cart-icon">
+            <img src={assets.cart_icon} alt="" className="icon"/>
+            <div className="dot"></div>
+        </div>
+        <button>Sign in</button>
+        </div> 
+    </div>
+  )
+}
+
+export default Navbar
